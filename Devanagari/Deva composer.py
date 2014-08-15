3ff9a25f7bf0d79fbff2cfb08ab8251806579760
Juan <juandelperal@gmail.com>
2014-08-15 10:41:00 -0300

    added Deva composer

---
+++ Devanagari/Deva composer.py
@@ -0,0 +1,27 @@
+#MenuTitle: Deva composer
+# -*- coding: utf-8 -*-
+__doc__="""
+Makes a new tab with the characters needed to type the selected conjuncts or halfforms.
+"""
+
+import GlyphsApp
+
+Font = Glyphs.font
+selectedGlyphs = [ x.parent for x in Font.selectedLayers ]
+
+outputString = ''
+
+# Conjuncts
+for thisGlyph in selectedGlyphs:
+	if thisGlyph.script == 'devanagari' and thisGlyph.category =='Letter' and thisGlyph.subCategory =='Ligature':
+		outputString += '/' + thisGlyph.name.replace("_","a-deva/halant-deva/") + '  '
+
+# Halform
+for thisGlyph in selectedGlyphs:
+	if thisGlyph.script == 'devanagari' and thisGlyph.category =='Letter' and thisGlyph.subCategory =='Halfform':
+		outputString += '/' + thisGlyph.name.replace("-deva","a-deva/halant-deva") + '  '
+
+
+# print outputString
+
+Glyphs.currentDocument.windowController().performSelectorOnMainThread_withObject_waitUntilDone_( "addTabWithString:", outputString, True )
