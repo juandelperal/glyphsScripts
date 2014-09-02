d4f5dfc2489f454a0cc93b617158209d737b0121
Juan <juandelperal@gmail.com>
2014-09-02 10:57:32 -0300

    simplified iMetralladora

--- Devanagari/iMetralladora.py
+++ Devanagari/iMetralladora.py
@@ -1,37 +1,21 @@
 #MenuTitle: iMetralladora
 # -*- coding: utf-8 -*-
 __doc__="""
-Combine selected glyphs in a new Tab with all iMatra relatives characters. Useful for Devanagari fonts. Warning: maybe it would blow up your computer.
+Combine selected with iMatra character. Useful for test 'pres' feature for Devanagari fonts
 """
 import GlyphsApp
-import re
 
 Doc = Glyphs.currentDocument
 Font = Glyphs.font
 selectedLayers = Font.selectedLayers
 
-imatras = [ l for l in Font.glyphs if re.search("iMatra-deva", l.name) ]
+interCharacter = "/iMatra-deva"
 
-try:
-	Glyphs.clearLog()
-	# Glyphs.showMacroWindow()
-except:
-	pass
+outputString = ""
 
-outputString = ''
-
-Font.disableUpdateInterface()
-for glyph in imatras:
-	outputString += glyph.name + ': \n'
-
-	for thisLayer in selectedLayers:
-		matra = '/' + glyph.name
-		thisGlyphName = '/' + thisLayer.parent.name
-		outputString += matra + thisGlyphName +'\n'
-
-	outputString += '\n'
+for thisLayer in selectedLayers:
+	thisGlyphName = thisLayer.parent.name
+	outputString += interCharacter + '/'+ thisGlyphName + '\n'
 
 
 Doc.windowController().performSelectorOnMainThread_withObject_waitUntilDone_( "addTabWithString:", outputString, True )
-
-Font.enableUpdateInterface()
